022d494ca1a9934992ee3000a780d7b1906c1284
SebastianLozanoValle <90271134+SebastianLozanoValle@users.noreply.github.com>
2023-11-02 17:07:41 -0500

    correcciones etiquetas a

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,30 @@ const showHideMenu = () =>{
     console.log('se activo el boton')
 
 }
+
+const contactForm = document.getElementById('contact-form'),
+contactMessage = document.getElementById('contact-message');
+
+const sendEmail = (e) =>{
+    e.preventDefault();
+
+    //serviceID - templateID - #form -publickey
+    emailjs.sendForm('service_2ufaels', 'template_4c1crvu', '#contact-form', 'vgO5yJZpYTrDRc7W7')
+    .then(() => {
+        //Show sent message
+        contactMessage.textContent = 'Message sent succefully ✅'
+
+        //Remove message after five seconds
+        setTimeout(() => {
+            contactMessage.textContent = '';
+        }, 5000);
+
+        //Clear input files
+        contactForm.reset();
+    }, () => {
+        //Show error message
+        contactMessage.textContent = 'Message not sent (service error) ❌'
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail);
